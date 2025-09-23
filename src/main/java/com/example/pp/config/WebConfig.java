d3620dd532bf22d16de5e8326649e5bc8f631e17
sssiion <rta2752775@gmail.com>
2025-09-24 07:02:17 +0900

    d
    d

--- src/main/java/com/example/pp/config/WebConfig.java
+++ src/main/java/com/example/pp/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig  implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // /api/ 로 시작하는 모든 요청에 대해
                 .allowedOrigins("https://ganglngreact-production-fb31.up.railway.app") // http://localhost:3000 (React 개발서버)의 요청을 허용
-                .allowedOrigins("https://localhost:8080")
+                .allowedOrigins("https://localhost:8080") //https://ppfront-production.up.railway.app/
+                .allowedOrigins("https://ppfront-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowCredentials(true);
     }
